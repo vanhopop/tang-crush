69d58239dbc2e0f248ddffeccc565b5159e2573a
vanhopop <89053180+vanhopop@users.noreply.github.com>
2021-08-17 09:47:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Đen của anh à!',
-    introDesc: `em có biết là tai nạn lớn nhất cuộc đời anh
-    chính là ngã vào lòng em không ',
+    introTitle: 'Đen à!',
+    introDesc: `em có biết là tai nạn lớn nhất của đời anh 
+    chính là ngã vào lòng em`,
     btnIntro: 'hihi',
-    title: ' Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: ' Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'yêu anh không <33',
-    btnNo: 'nói không thử xem !!! :3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là yêu anh <33',
+    btnNo: 'Không, Anh bạn à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh iu <3',
-    reply: 'em yêu em bé khổng lồ của e lắm <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 8h xem phim với a nhé babi.',
-    btnAccept: 'vâng ạ <3',
-    messLink: 'https://www.facebook.com/messages/t/100010187323850' 
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu anh nhiều như này nè <33333333',
+    mess: 'Anh biết mà, hihi a yêu bé lắm 😘😘',
+    messDesc: 'Tối nay 8h xem phim cùng nhé công chúa.',
+    btnAccept: ' vâng, thưa chồng <3',
+    messLink: 'https://www.facebook.com/messages/t/100010187323850' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
